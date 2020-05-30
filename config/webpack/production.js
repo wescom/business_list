9a1df870fe4c2df63bce87da73f87cb885458c5f
Steve Hoffmann <shoffmann@bendbulletin.com>
2020-05-30 10:14:15 -0700

    trying to get javascript to work in production

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,16 +1,5 @@
-#process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-#const environment = require('./environment')
+const environment = require('./environment')
 
-#module.exports = environment.toWebpackConfig()
-
-
-const { environment } = require('@rails/webpacker')
-var webpack = require('webpack');
-environment.plugins.append(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-  })
-)
-module.exports = environment
+module.exports = environment.toWebpackConfig()
