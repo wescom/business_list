da0d7ec865d95186e80f89e6d01c3dd8940072f8
Steve Hoffmann <shoffmann@bendbulletin.com>
2020-06-03 16:01:24 -0700

    trying to fix javascript not working

--- config/deploy.rb
+++ config/deploy.rb
@@ -117,7 +117,7 @@ namespace :deploy do
     end
   end
 
-  after "deploy:updated", "webpack:build"
+#  after "deploy:updated", "webpack:build"
   before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
