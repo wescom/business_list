2573b96882c91d06673fbe67b523db69523cb81c
Steve Hoffmann <shoffmann@bendbulletin.com>
2020-06-21 21:35:08 -0700

    once again, trying to get javascriptworking in prod

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+require("turbolinks")
 require("@rails/activestorage").start()
 require("channels")
 
