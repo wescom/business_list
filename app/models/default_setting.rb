751ad64e4bbf9520dca819b68465e0b4d7b3ee3c
Steve Hoffmann <shoffmann@bendbulletin.com>
2020-07-29 16:50:50 -0700

    moving home page images to uploads folder

--- app/models/default_setting.rb
+++ app/models/default_setting.rb
@@ -6,8 +6,8 @@ class DefaultSetting < ApplicationRecord
       :medium => ["600x600>",:jpg],
       :large => ["1000x1000>",:jpg]
     },
-    :url => "/images/homepage_images/:id/:style_:basename.:extension",  
-    :path => ":rails_root/public/images/homepage_images/:id/:style_:basename.:extension",
+    :url => "/uploads/homepage_images/:id/:style_:basename.:extension",  
+    :path => ":rails_root/uploads/images/homepage_images/:id/:style_:basename.:extension",
     :default_url => '/images/no-image.jpg'
 
     validates_attachment_content_type :homepage_banner_image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
@@ -18,8 +18,8 @@ class DefaultSetting < ApplicationRecord
         :medium => ["600x300>",:jpg],
         :large => ["1000x700>",:jpg]
       },
-      :url => "/images/homepage_images/:id/:style_:basename.:extension",  
-      :path => ":rails_root/public/images/homepage_images/:id/:style_:basename.:extension",
+      :url => "/uploads/homepage_images/:id/:style_:basename.:extension",  
+      :path => ":rails_root/public/uploads/homepage_images/:id/:style_:basename.:extension",
       :default_url => '/images/no-image.jpg'
 
       validates_attachment_content_type :homepage_general_instruction_image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
@@ -30,8 +30,8 @@ class DefaultSetting < ApplicationRecord
           :medium => ["600x300>",:jpg],
           :large => ["1000x500>",:jpg]
         },
-        :url => "/images/homepage_images/:id/:style_:basename.:extension",  
-        :path => ":rails_root/public/images/homepage_images/:id/:style_:basename.:extension",
+        :url => "/uploads/homepage_images/:id/:style_:basename.:extension",  
+        :path => ":rails_root/public/uploads/homepage_images/:id/:style_:basename.:extension",
         :default_url => '/images/no-image.jpg'
 
         validates_attachment_content_type :homepage_registration_image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
